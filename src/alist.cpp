e92731c2771ddce735a7d15b1a060b1916bd96ba
Brian Chin <brian.chin@gmail.com>
2025-01-30 08:38:02 -0800

    Move ANode to the alist.cpp/hpp file

--- src/alist.cpp
+++ src/alist.cpp
@@ -15,6 +15,26 @@ bool shrink;
 bool noOptimize;
 
 ///////////////////////////////////////////////////
+// Class ANode
+///////////////////////////////////////////////////
+
+// The flag addNodesToList is set during the optimization phase so that new
+// nodes to replace old ones are not automatically added to the current list.
+
+size_t ANode::size() { return 0; }
+
+size_t ANode::setOffset(size_t ofs) {
+  offset = ofs;
+  return ofs + size();
+}
+
+void ANode::emit(OutputFile*) {}
+
+void ANode::list() {}
+
+bool ANode::optimize() { return false; }
+
+///////////////////////////////////////////////////
 // Class AListIter
 ///////////////////////////////////////////////////
 ANode* AListIter::get() const { return (ANode*)iter_.get(); }
