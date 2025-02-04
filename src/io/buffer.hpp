4ec85a53643735390c1dd9342e09f4433d7b2b95
Brian Chin <brian.chin@gmail.com>
2025-02-03 23:23:05 -0800

    Add buffer library

---
+++ src/io/buffer.hpp
@@ -0,0 +1,28 @@
+#ifndef IO_BUFFER_HPP
+#define IO_BUFFER_HPP
+
+#include <cstdio>
+#include <string_view>
+
+namespace io {
+
+class ReadBuffer {
+ public:
+  static std::unique_ptr<ReadBuffer> FromFile(FILE* file);
+  static std::unique_ptr<ReadBuffer> FromString(std::string str);
+
+  ReadBuffer() = default;
+  ReadBuffer(const ReadBuffer&) = delete;
+  virtual ~ReadBuffer() = default;
+
+  virtual int GetLineIndex() const = 0;
+  // Returns the current line, including the trailing newline (if any).
+  // If not at end, will always have at least one character in the string.
+  virtual std::string_view CurrLine() const = 0;
+  virtual void AdvanceLine() = 0;
+  virtual bool IsAtEnd() const = 0;
+};
+
+}  // namespace io
+
+#endif
---
+++ src/io/buffer_test.cpp
@@ -0,0 +1,35 @@
+#include "src/io/buffer.hpp"
+
+#include <gtest/gtest.h>
+
+namespace io {
+namespace {
+
+TEST(ReadBufferTest, FindNewline) {
+  auto str_buffer = ReadBuffer::FromString(
+      "abc\n"
+      "def\r"
+      "ghi\r\n"
+      "jkl");
+
+  // First line is loaded.
+  EXPECT_EQ(str_buffer->CurrLine(), "abc\n");
+
+  // Curr line does not advance.
+  EXPECT_EQ(str_buffer->CurrLine(), "abc\n");
+
+  str_buffer->AdvanceLine();
+  EXPECT_EQ(str_buffer->CurrLine(), "def\r");
+
+  str_buffer->AdvanceLine();
+  EXPECT_EQ(str_buffer->CurrLine(), "ghi\r\n");
+
+  str_buffer->AdvanceLine();
+  EXPECT_EQ(str_buffer->CurrLine(), "jkl");
+  EXPECT_FALSE(str_buffer->IsAtEnd());
+  str_buffer->AdvanceLine();
+  EXPECT_EQ(str_buffer->CurrLine(), "");
+  EXPECT_TRUE(str_buffer->IsAtEnd());
+}
+}  // namespace
+}  // namespace io
