5ec8eb3c5e3e6ae4b44d262730be72004da97cb5
Brian Chin <brian.chin@gmail.com>
2025-02-05 12:59:44 -0800

    Split compile.hpp

--- scic/compile.hpp
+++ scic/compile.hpp
@@ -7,12 +7,11 @@
 
 #include "scic/alist.hpp"
 
-struct PNode;
 struct ANode;
 class Symbol;
 struct Object;
+struct PNode;
 
-//	compile.cpp
 void CompileProc(AList* curList, PNode* pn);
 void CompileExpr(AOpList* curList, PNode* pn);
 void MakeBranch(AOpList* curList, uint8_t type, ANode*, Symbol* target);
@@ -20,14 +19,5 @@ void MakeDispatch(int maxNum);
 void MakeObject(Object* theObj);
 void MakeText();
 void MakeLabel(AOpList* curList, Symbol* sym);
-
-//	loop.cpp
-void MakeWhile(AOpList* curList, PNode*);
-void MakeRepeat(AOpList* curList, PNode*);
-void MakeFor(AOpList* curList, PNode*);
-void MakeBreak(AOpList* curList, PNode*);
-void MakeBreakIf(AOpList* curList, PNode*);
-void MakeContinue(AOpList* curList, PNode*);
-void MakeContIf(AOpList* curList, PNode*);
 
 #endif
---
+++ scic/loop.hpp
@@ -0,0 +1,16 @@
+#ifndef LOOP_HPP
+#define LOOP_HPP
+
+#include "scic/alist.hpp"
+
+struct PNode;
+
+void MakeWhile(AOpList* curList, PNode*);
+void MakeRepeat(AOpList* curList, PNode*);
+void MakeFor(AOpList* curList, PNode*);
+void MakeBreak(AOpList* curList, PNode*);
+void MakeBreakIf(AOpList* curList, PNode*);
+void MakeContinue(AOpList* curList, PNode*);
+void MakeContIf(AOpList* curList, PNode*);
+
+#endif
