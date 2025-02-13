c8eb5d627ef75dec1311049acefa8c22aef7e076
Brian Chin <brian.chin@gmail.com>
2025-02-13 09:23:03 -0800

    Add choice_matchers library

---
+++ util/choice_matchers.hpp
@@ -0,0 +1,51 @@
+#ifndef UTIL_CHOICE_MATCHERS_HPP
+#define UTIL_CHOICE_MATCHERS_HPP
+
+#include <ostream>
+
+#include "gtest/gtest.h"
+#include "util/types/name.hpp"
+
+namespace util {
+
+namespace internal {
+template <class ChoiceT>
+class ChoiceOfImpl {
+ public:
+  explicit ChoiceOfImpl(testing::Matcher<ChoiceT> const& matcher)
+      : matcher_(matcher) {}
+
+  void DescribeTo(std::ostream* os) const {
+    *os << "is choice of type " << TypeName<ChoiceT>() << " that ";
+    matcher_.DescribeTo(os);
+  }
+
+  void DescribeNegationTo(std::ostream* os) const {
+    *os << "is not a choice of type " << TypeName<ChoiceT>() << " that ";
+    matcher_.DescribeTo(os);
+  }
+
+  template <class T>
+  bool MatchAndExplain(T const& value, testing::MatchResultListener* listener) const {
+    if (!value.template has<ChoiceT>()) {
+      *listener << "is not a choice of type " << TypeName<ChoiceT>();
+      return false;
+    }
+    return matcher_.MatchAndExplain(value.template as<ChoiceT>(), listener);
+  }
+
+ private:
+  testing::Matcher<ChoiceT> matcher_;
+};
+
+}  // namespace internal
+
+template <class ChoiceT>
+testing::PolymorphicMatcher<internal::ChoiceOfImpl<ChoiceT>> ChoiceOf(
+    testing::Matcher<ChoiceT> const& matcher) {
+  return testing::MakePolymorphicMatcher(
+      internal::ChoiceOfImpl<ChoiceT>(matcher));
+}
+
+}  // namespace util
+#endif
---
+++ util/choice_matchers_test.cpp
@@ -0,0 +1,27 @@
+#include "util/choice_matchers.hpp"
+
+#include <string>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "util/choice.hpp"
+
+namespace util {
+namespace {
+
+using testing::_;
+using testing::Not;
+
+class SimpleValue : public ChoiceBase<SimpleValue, int, std::string> {
+  using ChoiceBase::ChoiceBase;
+};
+
+TEST(ChoiceTest, Basic) {
+  SimpleValue choice = SimpleValue(42);
+  EXPECT_THAT(choice, ChoiceOf<int>(_));
+  EXPECT_THAT(choice, ChoiceOf<int>(42));
+  EXPECT_THAT(choice, Not(ChoiceOf<int>(12)));
+  EXPECT_THAT(choice, Not(ChoiceOf<std::string>(_)));
+}
+}  // namespace
+}  // namespace util
