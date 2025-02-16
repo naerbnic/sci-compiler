c8877b8941a8c637a3b5b03e587ecae65cc90fc6
Brian Chin <brian.chin@gmail.com>
2025-02-16 15:45:51 -0800

    Extract code to build the ItemRefs struct, to make futher passes simpler

--- scic/sem/passes/initial_pass.cpp
+++ scic/sem/passes/initial_pass.cpp
@@ -1,7 +1,7 @@
 #include "scic/sem/passes/initial_pass.hpp"
 
 #include <map>
-#include <optional>
+#include <stdexcept>
 #include <utility>
 #include <vector>
 
@@ -13,59 +13,29 @@
 namespace sem::passes {
 namespace {
 
+template <class C>
+auto GetSingleton(C&& value) {
+  auto begin_it = std::begin(value);
+  auto end_it = std::end(value);
+  if (begin_it == end_it) {
+    throw std::runtime_error("Expected exactly one element. Got zero");
+  }
+
+  // We want to handle the case when the value is a reference, and when it's
+  // a value type.
+  using ValueType = decltype(*begin_it);
+
+  // This should get and keep a forwarding reference to the value.
+  auto&& result = *begin_it;
+  ++begin_it;
+  if (begin_it != end_it) {
+    throw std::runtime_error("Expected exactly one element. Got more than one");
+  }
+
+  return std::forward<ValueType>(result);
+}
+
 namespace sci = parsers::sci;
-
-// A range of variable indexes.
-//
-// A simple variable has a single index, but an array has a range of indexes.
-// This represents the range [start, end).
-struct IndexRange {
-  int start;
-  int end;
-};
-
-struct ModuleVars {
-  parsers::sci::ModuleVarsDef::Kind kind;
-  std::map<SymbolId, IndexRange> var_decls;
-};
-
-struct ExternIndex {
-  int module_id;
-  int index;
-};
-
-struct ClassDeclInfo {};
-
-struct ClassDefInfo {
-  parsers::sci::ClassDef::Kind kind;
-};
-
-struct ProcInfo {};
-
-struct PassState {
-  std::optional<int> script_num;
-  std::map<SymbolId, int> global_decls;
-  std::optional<ModuleVars> module_vars;
-  std::map<SymbolId, ExternIndex> extern_decls;
-  std::map<SelectorId, int> selectors;
-  std::map<SymbolId, ClassDeclInfo> class_decls;
-  std::map<SymbolId, ClassDefInfo> class_defs;
-  std::map<SymbolId, ProcInfo> procedures;
-};
-
-struct ItemRefs {
-  std::vector<sci::ScriptNumDef const*> script_nums;
-  std::vector<sci::PublicDef const*> publics;
-  std::vector<sci::PublicDef::Entry const*> public_entries;
-  std::vector<sci::ExternDef::Entry const*> externs;
-  std::vector<sci::GlobalDeclDef::Entry const*> global_decls;
-  std::vector<sci::ModuleVarsDef const*> module_vars;
-  std::vector<sci::ModuleVarsDef::Entry const*> module_vars_entries;
-  std::vector<sci::ProcDef const*> procedures;
-  std::vector<sci::ClassDef const*> class_defs;
-  std::vector<sci::ClassDecl const*> class_decls;
-  std::vector<sci::SelectorsDecl::Entry const*> selectors;
-};
 
 std::pair<SymbolId, IndexRange> ParseVarDef(sci::VarDef const& var,
                                             int base_index) {
@@ -83,20 +53,13 @@ std::pair<SymbolId, IndexRange> ParseVarDef(sci::VarDef const& var,
 
 }  // namespace
 
-ModuleEnvironment RunInitialPass(absl::Span<parsers::sci::Item const> items,
-                                 diag::DiagnosticsSink* sink) {
-  PassState state;
+ItemRefs ExtractItemRefs(absl::Span<parsers::sci::Item const> items) {
   ItemRefs refs;
 
   for (auto const& item : items) {
     item.visit(
         [&](sci::ScriptNumDef const& item) {
           refs.script_nums.push_back(&item);
-          if (state.script_num) {
-            sink->Error("Duplicate script number definition.");
-            return;
-          }
-          state.script_num = item.script_num().value();
         },
         [&](sci::PublicDef const& item) {
           refs.publics.push_back(&item);
@@ -107,124 +70,149 @@ ModuleEnvironment RunInitialPass(absl::Span<parsers::sci::Item const> items,
         [&](sci::ExternDef const& item) {
           for (auto const& entry : item.entries()) {
             refs.externs.push_back(&entry);
-            auto sym_id = SymbolId::Create(entry.name.value());
-            auto index = entry.index.value();
-            auto module_id = entry.module_num.value();
-
-            // Maybe we should collect all of the values before we do error
-            // resolution.
-            if (auto it = state.extern_decls.find(sym_id);
-                it != state.extern_decls.end()) {
-              if (it->second.module_id != module_id ||
-                  it->second.index != index) {
-                sink->Error(
-                    "Duplicate external declaration with mismatching indexes.");
-              }
-              continue;
-            }
-            state.extern_decls.emplace(std::move(sym_id),
-                                       ExternIndex{
-                                           .module_id = module_id,
-                                           .index = index,
-                                       });
           }
         },
         [&](sci::GlobalDeclDef const& item) {
           for (auto const& entry : item.entries()) {
             refs.global_decls.push_back(&entry);
-            // We have to parse the vardef, to get the name and the size.
-            auto index = entry.index.value();
-            auto [sym_id, range] = ParseVarDef(entry.name, index);
-
-            if (state.global_decls.contains(sym_id)) {
-              sink->Error("Duplicate global declaration.");
-              continue;
-            }
-
-            state.global_decls.emplace(std::move(sym_id), index);
           }
         },
         [&](sci::ModuleVarsDef const& item) {
           refs.module_vars.push_back(&item);
-          if (state.module_vars) {
-            sink->Error("Duplicate module variable declaration.");
-            return;
-          }
-
-          ModuleVars module_vars{
-              .kind = item.kind(),
-          };
-
           for (auto const& entry : item.entries()) {
             refs.module_vars_entries.push_back(&entry);
-            auto index = entry.index.value();
-            auto [sym_id, range] = ParseVarDef(entry.name, index);
-
-            if (module_vars.var_decls.contains(sym_id)) {
-              sink->Error("Duplicate module variable declaration.");
-              continue;
-            }
-
-            module_vars.var_decls.emplace(std::move(sym_id), range);
           }
         },
-        [&](sci::ProcDef const& item) {
-          refs.procedures.push_back(&item);
-          auto sym_id = SymbolId::Create(item.name().value());
-          if (state.procedures.contains(sym_id)) {
-            sink->Error("Duplicate procedure definition.");
-            return;
-          }
-          state.procedures.emplace(std::move(sym_id), ProcInfo{});
-        },
-        [&](sci::ClassDef const& item) {
-          refs.class_defs.push_back(&item);
-          auto sym_id = SymbolId::Create(item.name().value());
-          if (state.class_defs.contains(sym_id)) {
-            sink->Error("Duplicate class definition.");
-            return;
-          }
-          state.class_defs.emplace(std::move(sym_id), ClassDefInfo{
-                                                          .kind = item.kind(),
-                                                      });
-        },
-        [&](sci::ClassDecl const& item) {
-          refs.class_decls.push_back(&item);
-          auto sym_id = SymbolId::Create(item.name().value());
-          if (state.class_decls.contains(sym_id)) {
-            sink->Error("Duplicate class declaration.");
-            return;
-          }
-          state.class_decls.emplace(std::move(sym_id), ClassDeclInfo{});
-        },
+        [&](sci::ProcDef const& item) { refs.procedures.push_back(&item); },
+        [&](sci::ClassDef const& item) { refs.class_defs.push_back(&item); },
+        [&](sci::ClassDecl const& item) { refs.class_decls.push_back(&item); },
         [&](sci::SelectorsDecl const& item) {
           for (auto const& entry : item.selectors()) {
             refs.selectors.push_back(&entry);
-            auto sym_id = SelectorId::Create(entry.name.value());
-            auto index = entry.id.value();
-
-            if (state.selectors.contains(sym_id)) {
-              sink->Error("Duplicate selector declaration.");
-              continue;
-            }
-
-            state.selectors.emplace(std::move(sym_id), index);
           }
         });
   }
 
-  std::optional<int> script_num;
+  return refs;
+}
 
-  if (refs.script_nums.size() > 1) {
+ModuleEnvironment RunInitialPass(ItemRefs const& items,
+                                 diag::DiagnosticsSink* sink) {
+  ModuleEnvironment state;
+
+  if (items.script_nums.size() > 1) {
     sink->Error("Multiple script number definitions.");
-  } else if (refs.script_nums.empty()) {
+  } else if (items.script_nums.empty()) {
     sink->Error("Missing script number definition.");
   } else {
-    script_num = refs.script_nums.front()->script_num().value();
+    state.script_num = items.script_nums.front()->script_num().value();
   }
 
-  
+  for (auto const* entry : items.externs) {
+    auto sym_id = SymbolId::Create(entry->name.value());
+    auto index = entry->index.value();
+    auto module_id = entry->module_num.value();
 
-  return ModuleEnvironment{};
+    // Maybe we should collect all of the values before we do error
+    // resolution.
+    if (auto it = state.extern_decls.find(sym_id);
+        it != state.extern_decls.end()) {
+      if (it->second.module_id != module_id || it->second.index != index) {
+        sink->Error("Duplicate external declaration with mismatching indexes.");
+      }
+      continue;
+    }
+    state.extern_decls.emplace(std::move(sym_id), ExternIndex{
+                                                      .module_id = module_id,
+                                                      .index = index,
+                                                  });
+  }
+
+  for (auto const* entry : items.global_decls) {
+    // We have to parse the vardef, to get the name and the size.
+    auto index = entry->index.value();
+    auto [sym_id, range] = ParseVarDef(entry->name, index);
+
+    if (state.global_decls.contains(sym_id)) {
+      sink->Error("Duplicate global declaration.");
+      continue;
+    }
+
+    state.global_decls.emplace(std::move(sym_id), index);
+  }
+
+  if (items.module_vars.size() > 1) {
+    sink->Error("Duplicate module variable declaration.");
+  } else if (!items.module_vars.empty()) {
+    auto const* module_vars = GetSingleton(items.module_vars);
+
+    if (state.script_num == 0) {
+      if (module_vars->kind() != sci::ModuleVarsDef::GLOBAL) {
+        sink->Error("Only global module variables are allowed in the kernel.");
+      }
+    } else if (state.script_num) {
+      if (module_vars->kind() != sci::ModuleVarsDef::LOCAL) {
+        sink->Error(
+            "Only local module variables are allowed in "
+            "non-kernel scripts.");
+      }
+    }
+
+    for (auto const* entry : items.module_vars_entries) {
+      auto index = entry->index.value();
+      auto [sym_id, range] = ParseVarDef(entry->name, index);
+
+      if (state.var_decls.contains(sym_id)) {
+        sink->Error("Duplicate module variable declaration.");
+        continue;
+      }
+
+      state.var_decls.emplace(std::move(sym_id), range);
+    }
+  }
+
+  for (auto const* entry : items.procedures) {
+    auto sym_id = SymbolId::Create(entry->name().value());
+    if (state.procedures.contains(sym_id)) {
+      sink->Error("Duplicate procedure definition.");
+      continue;
+    } else {
+      state.procedures.emplace(std::move(sym_id), ProcInfo{});
+    }
+  }
+
+  for (auto const* entry : items.class_defs) {
+    auto sym_id = SymbolId::Create(entry->name().value());
+    if (state.class_defs.contains(sym_id)) {
+      sink->Error("Duplicate class definition.");
+      continue;
+    }
+    state.class_defs.emplace(std::move(sym_id), ClassDefInfo{
+                                                    .kind = entry->kind(),
+                                                });
+  }
+
+  for (auto const* class_decl : items.class_decls) {
+    auto sym_id = SymbolId::Create(class_decl->name().value());
+    if (state.class_decls.contains(sym_id)) {
+      sink->Error("Duplicate class declaration.");
+      continue;
+    }
+    state.class_decls.emplace(std::move(sym_id), ClassDeclInfo{});
+  }
+
+  for (auto const* selector : items.selectors) {
+    auto sym_id = SelectorId::Create(selector->name.value());
+    auto index = selector->id.value();
+
+    if (state.selector_decls.contains(sym_id)) {
+      sink->Error("Duplicate selector declaration.");
+      continue;
+    }
+
+    state.selector_decls.emplace(std::move(sym_id), index);
+  }
+
+  return state;
 }
 }  // namespace sem::passes
--- scic/sem/passes/initial_pass.hpp
+++ scic/sem/passes/initial_pass.hpp
@@ -1,15 +1,66 @@
 #ifndef SEM_PASSES_INITIAL_PASS_HPP
 #define SEM_PASSES_INITIAL_PASS_HPP
 
+#include <map>
+#include <vector>
+
 #include "absl/types/span.h"
 #include "scic/diagnostics/diagnostics.hpp"
 #include "scic/parsers/sci/ast.hpp"
+#include "scic/sem/symbol.hpp"
 
 namespace sem::passes {
 
-class ModuleEnvironment {};
+struct ItemRefs {
+  std::vector<parsers::sci::ScriptNumDef const*> script_nums;
+  std::vector<parsers::sci::PublicDef const*> publics;
+  std::vector<parsers::sci::PublicDef::Entry const*> public_entries;
+  std::vector<parsers::sci::ExternDef::Entry const*> externs;
+  std::vector<parsers::sci::GlobalDeclDef::Entry const*> global_decls;
+  std::vector<parsers::sci::ModuleVarsDef const*> module_vars;
+  std::vector<parsers::sci::ModuleVarsDef::Entry const*> module_vars_entries;
+  std::vector<parsers::sci::ProcDef const*> procedures;
+  std::vector<parsers::sci::ClassDef const*> class_defs;
+  std::vector<parsers::sci::ClassDecl const*> class_decls;
+  std::vector<parsers::sci::SelectorsDecl::Entry const*> selectors;
+};
 
-ModuleEnvironment RunInitialPass(absl::Span<parsers::sci::Item const> items,
+ItemRefs ExtractItemRefs(absl::Span<parsers::sci::Item const> items);
+
+// A range of variable indexes.
+//
+// A simple variable has a single index, but an array has a range of indexes.
+// This represents the range [start, end).
+struct IndexRange {
+  int start;
+  int end;
+};
+
+struct ExternIndex {
+  int module_id;
+  int index;
+};
+
+struct ClassDeclInfo {};
+
+struct ClassDefInfo {
+  parsers::sci::ClassDef::Kind kind;
+};
+
+struct ProcInfo {};
+
+struct ModuleEnvironment {
+  int script_num;
+  std::map<SymbolId, int> global_decls;
+  std::map<SymbolId, IndexRange> var_decls;
+  std::map<SymbolId, ExternIndex> extern_decls;
+  std::map<SelectorId, int> selector_decls;
+  std::map<SymbolId, ClassDeclInfo> class_decls;
+  std::map<SymbolId, ClassDefInfo> class_defs;
+  std::map<SymbolId, ProcInfo> procedures;
+};
+
+ModuleEnvironment RunInitialPass(ItemRefs const& items,
                                  diag::DiagnosticsSink* sink);
 
 }  // namespace sem::passes
