660f268ebf6d5b97a68e95f7f122856bf4e92a32
Brian Chin <brian.chin@gmail.com>
2025-02-22 11:38:53 -0800

    Clean up build_code to create class and selector tables for a module

--- scic/sem/passes/build_code.cpp
+++ scic/sem/passes/build_code.cpp
@@ -1,10 +1,7 @@
 #include "scic/sem/passes/build_code.hpp"
 
-#include <algorithm>
-#include <cstddef>
 #include <memory>
 #include <optional>
-#include <type_traits>
 #include <utility>
 #include <vector>
 
@@ -13,49 +10,17 @@
 #include "absl/types/span.h"
 #include "scic/codegen/code_generator.hpp"
 #include "scic/parsers/sci/ast.hpp"
+#include "scic/sem/class_table.hpp"
 #include "scic/sem/common.hpp"
 #include "scic/sem/selector_table.hpp"
 #include "util/status/status_macros.hpp"
 #include "util/strings/ref_str.hpp"
-#include "util/types/choice.hpp"
-#include "util/types/overload.hpp"
 
 namespace sem::passes {
 
 using ::codegen::CodeGenerator;
 
-template <class... Fs>
-absl::Status StatusVisitItems(Items items, Fs&&... fs) {
-  for (auto const& item : items) {
-    auto status = item.visit(std::forward<Fs>(fs)...);
-    if (!status.ok()) {
-      return status;
-    }
-  }
-  return absl::OkStatus();
-}
-
-template <class T, class... Fs>
-auto VectorVisitItems(Items items, Fs&&... fs) {
-  std::vector<T> result;
-  for (auto const& item : items) {
-    auto overload = util::Overload(std::forward<Fs>(fs)...);
-    item.visit([&](auto&& value) {
-      auto inner_result = overload(value);
-      using ResultType = std::decay_t<decltype(inner_result)>;
-      if constexpr (std::ranges::range<ResultType>) {
-        std::ranges::move(std::move(inner_result), std::back_inserter(result));
-      } else {
-        static_assert(
-            std::is_same_v<ResultType, T>,
-            "Result type must be a range of items that are convertible to T");
-      }
-    });
-  }
-  return result;
-}
-
-absl::StatusOr<std::size_t> GetScriptId(Items items) {
+absl::StatusOr<ScriptNum> GetScriptId(Items items) {
   auto result = GetElemsOfTypes<ast::ScriptNumDef>(items);
 
   if (result.size() == 0) {
@@ -64,10 +29,10 @@ absl::StatusOr<std::size_t> GetScriptId(Items items) {
     return absl::InvalidArgumentError("Multiple script numbers defined");
   }
 
-  return result[0]->script_num().value();
+  return ScriptNum::Create(result[0]->script_num().value());
 }
 
-absl::StatusOr<std::unique_ptr<SelectorTable>> BuildFromItems(
+absl::StatusOr<std::unique_ptr<SelectorTable>> BuildSelectorTableFromItems(
     absl::Span<ast::Item const> items) {
   auto builder = SelectorTable::CreateBuilder();
   {
@@ -98,92 +63,6 @@ absl::StatusOr<std::unique_ptr<SelectorTable>> BuildFromItems(
   return builder->Build();
 }
 
-absl::StatusOr<ClassDeclTable> BuildClassDeclGraph(
-    SelectorTable const* sel_table, absl::Span<ast::Item const> items) {
-  auto classes = GetElemsOfTypes<ast::ClassDecl>(items);
-
-  ClassDeclTable class_decls;
-  // First, create all of the class declarations. We'll patch in the other
-  // values later.
-  for (auto const* class_decl : classes) {
-    std::size_t class_num = class_decl->class_num().value();
-
-    std::vector<ClassDecl::Property> properties;
-    for (auto const& prop : class_decl->properties()) {
-      auto entry = sel_table->LookupByName(prop.name.value());
-      if (!entry) {
-        return absl::InvalidArgumentError("Property not found");
-      }
-
-      if (!prop.value.has<ast::NumConstValue>()) {
-        return absl::InvalidArgumentError(
-            "Property value must be a number in a class declaration.");
-      }
-
-      ASSIGN_OR_RETURN(
-          auto value, ConvertToMachineWord(
-                          prop.value.as<ast::NumConstValue>().value().value()));
-
-      properties.push_back(ClassDecl::Property{
-          .name = prop.name,
-          .selector = entry,
-          .value = value,
-      });
-    }
-
-    std::vector<SelectorTable::Entry const*> methods;
-    for (auto const& method_name : class_decl->method_names().names) {
-      auto entry = sel_table->LookupByName(method_name.value());
-      if (!entry) {
-        return absl::InvalidArgumentError("Method not found");
-      }
-      methods.push_back(entry);
-    }
-
-    RETURN_IF_ERROR(class_decls.AddClass(
-        class_decl->name(), class_num, class_decl->script_num().value(),
-        std::move(properties), std::move(methods)));
-  }
-
-  // Patch in the super classes.
-  for (auto const* class_decl : classes) {
-    std::size_t class_num = class_decl->class_num().value();
-    std::optional<std::size_t> super_num;
-
-    if (class_decl->parent_num()) {
-      super_num = class_decl->parent_num()->value();
-    }
-
-    RETURN_IF_ERROR(class_decls.SetClassSuper(class_num, super_num));
-  }
-
-  return class_decls;
-}
-
-absl::StatusOr<std::vector<ast::TokenNode<util::RefStr>>> GatherNewSelectors(
-    SelectorTable const* sel_table, absl::Span<ast::Item const> items) {
-  auto classes = GetElemsOfTypes<ast::ClassDef>(items);
-  std::vector<ast::TokenNode<util::RefStr>> result;
-  for (auto const* class_def : classes) {
-    for (auto const& prop : class_def->properties()) {
-      result.push_back(prop.name);
-    }
-    for (auto const& method : class_def->methods()) {
-      result.push_back(method.name());
-    }
-  }
-
-  for (auto it = result.begin(); it != result.end();) {
-    if (sel_table->LookupByName(it->value())) {
-      it = result.erase(it);
-    } else {
-      ++it;
-    }
-  }
-
-  return result;
-}
-
 static codegen::LiteralValue AstConstValueToLiteralValue(
     CodeGenerator* codegen, ast::ConstValue const& value) {
   return value.visit(
@@ -195,47 +74,59 @@ static codegen::LiteralValue AstConstValueToLiteralValue(
       });
 }
 
-absl::StatusOr<ClassDefTable> BuildClassDefTable(
+absl::StatusOr<std::unique_ptr<ClassTable>> BuildClassTableFromItems(
     CodeGenerator* codegen, SelectorTable const* selectors,
-    ClassDeclTable const* class_decls, absl::Span<ast::Item const> items) {
-  auto classes = GetElemsOfTypes<ast::ClassDef>(items);
+    ScriptNum script_num, absl::Span<ast::Item const> items) {
+  auto builder = ClassTableBuilder::Create(selectors);
 
-  ClassDefTable class_defs;
-  for (auto const* classdef : classes) {
+  for (auto const* class_decl : GetElemsOfTypes<ast::ClassDecl>(items)) {
+    std::vector<ClassTableBuilder::Property> properties;
+    for (auto const& prop : class_decl->properties()) {
+      properties.push_back(ClassTableBuilder::Property{
+          .name = prop.name,
+          .value = AstConstValueToLiteralValue(codegen, prop.value),
+      });
+    }
+    std::vector<NameToken> methods;
+    for (auto const& method_name : class_decl->method_names().names) {
+      methods.push_back(method_name);
+    }
+    std::optional<ClassSpecies> super_species;
+    if (class_decl->parent_num()) {
+      super_species = ClassSpecies::Create(class_decl->parent_num()->value());
+    }
+    RETURN_IF_ERROR(builder->AddClassDecl(
+        class_decl->name(), ScriptNum::Create(class_decl->script_num().value()),
+        ClassSpecies::Create(class_decl->class_num().value()), super_species,
+        std::move(properties), std::move(methods)));
+  }
+
+  for (auto const* classdef : GetElemsOfTypes<ast::ClassDef>(items)) {
     auto const& name = classdef->name();
-    std::vector<ClassDef::Property> properties;
+    std::optional<NameToken> super_name;
+    if (classdef->parent()) {
+      super_name = *classdef->parent();
+    }
+    std::vector<ClassTableBuilder::Property> properties;
     for (auto const& prop : classdef->properties()) {
       auto const& prop_name = prop.name;
-      auto literal_value = AstConstValueToLiteralValue(codegen, prop.value);
-      auto const* selector = selectors->LookupByName(prop_name.value());
-      if (!selector) {
-        return absl::InvalidArgumentError("Selector not found");
-      }
-      properties.push_back(ClassDef::Property{
+      properties.push_back(ClassTableBuilder::Property{
           .name = prop_name,
-          .selector = selector,
-          .value = literal_value,
+          .value = AstConstValueToLiteralValue(codegen, prop.value),
       });
     }
 
-    std::vector<ClassDef::Method> methods;
+    std::vector<NameToken> methods;
     for (auto const& ast_method : classdef->methods()) {
-      auto const& method_name = ast_method.name();
-      auto const* selector = selectors->LookupByName(method_name.value());
-      if (!selector) {
-        return absl::InvalidArgumentError("Selector not found");
-      }
-      methods.push_back(ClassDef::Method{
-          .name = method_name,
-          .selector = selector,
-      });
+      methods.push_back(ast_method.name());
     }
 
-    auto const* class_decl = class_decls->LookupByName(name.value());
-    RETURN_IF_ERROR(class_defs.AddClassDef(
-        name, class_decl, std::move(properties), std::move(methods)));
+    RETURN_IF_ERROR(builder->AddClassDef(name, script_num, super_name,
+                                         std::move(properties),
+                                         std::move(methods)));
   }
-  return absl::UnimplementedError("Not yet implemented.");
+
+  return builder->Build();
 }
 
 }  // namespace sem::passes
--- scic/sem/passes/build_code.hpp
+++ scic/sem/passes/build_code.hpp
@@ -1,287 +1,31 @@
 #ifndef SEM_PASSES_BUILD_CODE_HPP
 #define SEM_PASSES_BUILD_CODE_HPP
 
-#include <cstddef>
-#include <cstdint>
-#include <functional>
-#include <map>
 #include <memory>
-#include <optional>
-#include <string_view>
-#include <utility>
-#include <vector>
 
-#include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/types/span.h"
-#include "scic/codegen/code_generator.hpp"
 #include "scic/parsers/sci/ast.hpp"
+#include "scic/sem/class_table.hpp"
 #include "scic/sem/common.hpp"
-#include "scic/sem/late_bound.hpp"
 #include "scic/sem/selector_table.hpp"
-#include "util/strings/ref_str.hpp"
-#include "util/types/choice.hpp"
 
 namespace sem::passes {
 
-class ClassDecl {
- public:
-  struct Property {
-    parsers::sci::TokenNode<util::RefStr> name;
-    SelectorTable::Entry const* selector;
-    std::uint16_t value;
-  };
-
-  ClassDecl(ast::TokenNode<util::RefStr> name, std::size_t species,
-            std::size_t script_num, std::vector<Property> properties,
-            std::vector<SelectorTable::Entry const*> methods)
-      : name_(std::move(name)),
-        species_(species),
-        script_num_(script_num),
-        properties_(std::move(properties)),
-        methods_(std::move(methods)) {}
-
-  ast::TokenNode<util::RefStr> const& get_name() const { return name_; }
-  std::size_t get_species() const { return species_; }
-  std::size_t get_script_num() const { return script_num_; }
-  ClassDecl const* get_super() const { return *super_; }
-
-  // Late binding setters. These should only be called once during the creation
-  // of the class graph.
-  void set_super(ClassDecl const* new_super) { super_.set(new_super); }
-
- private:
-  ast::TokenNode<util::RefStr> name_;
-  std::size_t species_;
-  std::size_t script_num_;
-  LateBound<ClassDecl const*> super_;
-
-  std::vector<Property> properties_;
-  std::vector<SelectorTable::Entry const*> methods_;
-};
-
-class ClassDeclTable {
- public:
-  ClassDeclTable() = default;
-
-  absl::Status AddClass(ast::TokenNode<util::RefStr> name, std::size_t species,
-                        std::size_t script_num,
-                        std::vector<ClassDecl::Property> properties,
-                        std::vector<SelectorTable::Entry const*> methods) {
-    if (table_.contains(species)) {
-      return absl::InvalidArgumentError("Class species already exists");
-    }
-
-    if (name_table_.contains(name.value())) {
-      return absl::InvalidArgumentError("Class name already exists");
-    }
-
-    auto new_decl =
-        std::make_unique<ClassDecl>(std::move(name), species, script_num,
-                                    std::move(properties), std::move(methods));
-    auto const* new_decl_ptr = new_decl.get();
-
-    table_.emplace(species, std::move(new_decl));
-    name_table_.emplace(new_decl_ptr->get_name().value(), new_decl_ptr);
-
-    return absl::OkStatus();
-  }
-
-  absl::Status SetClassSuper(std::size_t base_class,
-                             std::optional<std::size_t> super_num) {
-    auto it = table_.find(base_class);
-    if (it == table_.end()) {
-      return absl::FailedPreconditionError("Base class not found");
-    }
-    ClassDecl const* super = nullptr;
-    if (super_num) {
-      auto it = table_.find(*super_num);
-      if (it == table_.end()) {
-        return absl::InvalidArgumentError("Parent class not found");
-      }
-      super = it->second.get();
-    }
-    it->second->set_super(super);
-    return absl::OkStatus();
-  }
-
-  ClassDecl const* LookupBySpecies(std::size_t species) const {
-    auto it = table_.find(species);
-    if (it == table_.end()) {
-      return nullptr;
-    }
-    return it->second.get();
-  }
-
-  ClassDecl const* LookupByName(std::string_view species) const {
-    auto it = name_table_.find(species);
-    if (it == name_table_.end()) {
-      return nullptr;
-    }
-    return it->second;
-  }
-
- private:
-  std::map<std::size_t, std::unique_ptr<ClassDecl>> table_;
-  std::map<std::string_view, ClassDecl const*, std::less<>> name_table_;
-};
-
-class ClassDef {
- public:
-  struct Property {
-    ast::TokenNode<util::RefStr> name;
-    SelectorTable::Entry const* selector;
-    codegen::LiteralValue value;
-  };
-
-  struct Method {
-    ast::TokenNode<util::RefStr> name;
-    SelectorTable::Entry const* selector;
-  };
-
-  ClassDef(ast::TokenNode<util::RefStr> name, ClassDecl const* old_decl,
-           std::vector<Property> properties, std::vector<Method> methods)
-      : name_(std::move(name)),
-        old_decl_(old_decl),
-        properties_(std::move(properties)),
-        methods_(std::move(methods)) {}
-
-  ast::TokenNode<util::RefStr> const& token_name() const { return name_; }
-  util::RefStr const& name() const { return name_.value(); }
-  ClassDecl const* old_decl() const { return old_decl_; }
-  std::vector<Property> const& properties() const { return properties_; }
-  std::vector<Method> const& methods() const { return methods_; }
-
- private:
-  ast::TokenNode<util::RefStr> name_;
-  ClassDecl const* old_decl_;
-  // If this is non-empty, then this is a redefinition of a previous class.
-  LateBound<std::optional<
-      util::Choice<ClassDecl const*, ast::TokenNode<util::RefStr>>>>
-      super_;
-  std::vector<Property> properties_;
-  std::vector<Method> methods_;
-};
-
-class ClassDefTable {
- public:
-  absl::Status AddClassDef(ast::TokenNode<util::RefStr> name,
-                           ClassDecl const* class_decl,
-                           std::vector<ClassDef::Property> properties,
-                           std::vector<ClassDef::Method> methods) {
-    if (name_table_.contains(name.value())) {
-      return absl::InvalidArgumentError("Class name already exists");
-    }
-    auto new_decl = std::make_unique<ClassDef>(
-        std::move(name), class_decl, std::move(properties), std::move(methods));
-
-    name_table_.emplace(new_decl->name(), new_decl.get());
-    pending_classes_.emplace_back(std::move(new_decl));
-    return absl::OkStatus();
-  }
-
-  ClassDef const* LookupByName(std::string_view name) const {
-    auto it = name_table_.find(name);
-    if (it == name_table_.end()) {
-      return nullptr;
-    }
-    return it->second;
-  }
-
- private:
-  std::vector<std::unique_ptr<ClassDef>> pending_classes_;
-  std::map<std::size_t, std::unique_ptr<ClassDef>> classes_;
-  std::map<std::string_view, ClassDef const*, std::less<>> name_table_;
-};
-
-class ClassTableBuilder {
- public:
-  struct Property {
-    ast::TokenNode<util::RefStr> name;
-    SelectorTable::Entry const* selector;
-    // The value of a selector.
-    //
-    // In a class decl, this must be a number.
-    // In a class def, this can be any literal value.
-    codegen::LiteralValue value;
-  };
-
-  struct Method {
-    ast::TokenNode<util::RefStr> name;
-    SelectorTable::Entry const* selector;
-  };
-
-  absl::Status AddClassDecl(ast::TokenNode<util::RefStr> name,
-                            std::size_t species, std::size_t script_num,
-                            std::optional<std::size_t> super_num,
-                            std::vector<Property> properties,
-                            std::vector<Method> methods) {
-    for (auto const& prop : properties) {
-      if (!prop.value.has<int>()) {
-        return absl::InvalidArgumentError(
-            "Property value must be a number in a "
-            "class declaration.");
-      }
-    }
-    decls_.emplace_back(std::make_unique<ClassDecl>(ClassDecl{
-        .name = std::move(name),
-        .species = species,
-        .script_num = script_num,
-        .super_num = super_num,
-        .properties = std::move(properties),
-        .methods = std::move(methods),
-    }));
-    return absl::OkStatus();
-  }
-
-  absl::Status AddClassDef(ast::TokenNode<util::RefStr> name,
-                           std::size_t script_num,
-                           std::optional<util::RefStr> super,
-                           std::vector<Property> properties,
-                           std::vector<Method> methods) {
-    defs_.emplace_back(std::make_unique<ClassDef>(ClassDef{
-        .name = std::move(name),
-        .script_num = script_num,
-        .super = super,
-        .properties = std::move(properties),
-        .methods = std::move(methods),
-    }));
-    return absl::OkStatus();
-  }
-
- private:
-  struct ClassDecl {
-    ast::TokenNode<util::RefStr> name;
-    std::size_t species;
-    std::size_t script_num;
-    std::optional<std::size_t> super_num;
-
-    std::vector<Property> properties;
-    std::vector<Method> methods;
-  };
-
-  struct ClassDef {
-    ast::TokenNode<util::RefStr> name;
-    std::size_t script_num;
-    std::optional<util::RefStr> super;
-    std::vector<Property> properties;
-    std::vector<Method> methods;
-  };
-
-  std::vector<std::unique_ptr<ClassDef>> defs_;
-  std::vector<std::unique_ptr<ClassDecl>> decls_;
-};
-
 // Gets the script ID from the module with the given items.
-absl::StatusOr<std::size_t> GetScriptId(Items items);
+absl::StatusOr<ScriptNum> GetScriptId(Items items);
 
-// Collects all the selectors that are defined in a module, but have not
-// previously been declared.
-absl::StatusOr<std::vector<ast::TokenNode<util::RefStr>>> GatherNewSelectors(
-    SelectorTable const* sel_table, Items items);
+// Builds a new SelectorTable from the given items.
+//
+// The resulting table includes both the selectors previously declared in the
+// module, and any new selectors that were added in class definitions.
+absl::StatusOr<std::unique_ptr<SelectorTable>> BuildSelectorTableFromItems(
+    absl::Span<ast::Item const> items);
 
 // Collects all the class definitions from a module.
-absl::StatusOr<ClassDeclTable> BuildClassDeclGraph(
-    SelectorTable const* sel_table, absl::Span<ast::Item const> items);
+absl::StatusOr<std::unique_ptr<ClassTable>> BuildClassTable(
+    SelectorTable const* sel_table, ScriptNum script_num,
+    absl::Span<ast::Item const> items);
+
 };  // namespace sem::passes
 #endif
