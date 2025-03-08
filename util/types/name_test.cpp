72db95c5e26ef24ad44e070335e8818b35f4620c
Brian Chin <brian.chin@gmail.com>
2025-03-08 14:54:33 -0800

    Add namespace test for TypeName

--- util/types/name_test.cpp
+++ util/types/name_test.cpp
@@ -15,6 +15,7 @@ TEST(NameTest, InNamespaceWorks) { EXPECT_EQ(TypeName<Bar>(), "util::Bar"); }
 #else
 // In Windows for now, disable the test as I need to investigate the issue.
 TEST(NameTest, BasicWorks) { EXPECT_EQ(TypeName<Foo>(), "class Foo"); }
+TEST(NameTest, InNamespaceWorks) { EXPECT_EQ(TypeName<Bar>(), "class util::Bar"); }
 #endif
 
 }  // namespace
